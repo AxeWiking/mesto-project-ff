43da5e176b58aa2938a728a7eb0cd2399a088f77
Aleksei Iakovlev <al.iakovlev@innopolis.ru>
2025-01-06 20:20:03 +0500

    На ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
-// @todo: Темплейт карточки
+const defaultCardTitle = "Бермудский треугольник";
+const defaultCardLink = "#";
 
-// @todo: DOM узлы
+const content = document.querySelector('.content');
+const places = content.querySelector('.places');
 
-// @todo: Функция создания карточки
+function addCard(card, functionDelete = deleteCard) {
+  const titleValue = card.name || defaultCardTitle;
+  const linkValue = card.link || defaultCardLink;
 
-// @todo: Функция удаления карточки
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
+  cardItem.querySelector('.card__title').textContent = titleValue;
+  cardItem.querySelector('.card__image').src = linkValue;
+  cardItem.querySelector('.card__delete-button').addEventListener('click', functionDelete);
 
-// @todo: Вывести карточки на страницу
+  places.querySelector('.places__list').append(cardItem);
+}
+
+function deleteCard(event) {
+  const cardItem = event.target.closest('.card');
+  cardItem.remove();
+}
+
+initialCards.forEach(item => addCard(item, deleteCard));
